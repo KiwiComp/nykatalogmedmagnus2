7c890ac2b9bf5c6383518f4e64c0ddefc0afe9cd
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-10-16 20:46:45 +0200

    From local branch 1

--- src/Product.java
+++ src/Product.java
@@ -1,32 +1,49 @@
 public class Product {
-    private String name;
+    private String title;
     private double price;
-
+private boolean isGood;
     private String type;
 
-    public Product(String name, double price, String type) {
-        this.name = name;
+    public Product(String title, double price, String type, boolean isGood) {
+        this.title = title;
         this.price = price;
         this.type = type;
+        this.isGood = isGood;
 
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public double getPrice() {
         return price;
     }
 
+    public boolean isGood() {
+        return isGood;
+    }
+
+    public void setGood(boolean good) {
+        isGood = good;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     public String toString() {
         return "Product" +
-                "\nName: " + name +
+                "\nName: " + title +
                 "\nPrice: " + price;
     }
 
